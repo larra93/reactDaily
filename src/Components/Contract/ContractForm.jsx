4b0e3025217f8bed7df0d9f6fa582b00d4e5b5de
Felipe <felipelarra.araya@gmail.com>
2024-06-25 19:08:19 -0400

    WIP Contratos

--- src/Components/Contract/ContractForm.jsx
+++ src/Components/Contract/ContractForm.jsx
@@ -1,77 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { TextField, Button, Box, FormControl, Select, InputLabel, MenuItem, Chip, OutlinedInput, FormControlLabel, Checkbox } from '@mui/material';
+import React from 'react';
+import axios from 'axios';
+import { TextField, Button, Box, FormControl, InputLabel, Select, MenuItem, OutlinedInput, Chip } from '@mui/material';
 import { useForm, Controller } from 'react-hook-form';
 import { useTheme } from '@mui/material/styles';
 
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
-
-const getStyles = (name, personName, theme) => {
-  return {
-    fontWeight:
-      personName.indexOf(name) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium,
-  };
-};
-
-const ContractForm = ({ onSubmit }) => {
+const ContractForm = ({ onSubmit, users, companies }) => {
   const theme = useTheme();
-  const { register, handleSubmit, formState: { errors }, control, setValue } = useForm();
-  const [approver, setApprover] = useState([]);
-
-  // Datos estáticos para probar
-  const companies = [
-    { id: 1, name: 'Company A' },
-    { id: 2, name: 'Company B' },
-  ];
-
-  const users = [
-    { id: 1, name: 'Felipe Larraguibel - felipe@example.com' },
-    { id: 2, name: 'Diego Cataldo - diego@example.com' },
-  ];
-
-  useEffect(() => {
-    // Simulación de datos pre-rellenados para la prueba
-    const contract = {
-      NSAP: 'NSAP12345',
-      DEN: 'DEN12345',
-      project: 'Proyecto ABC',
-      API: 'API12345',
-      start_date: '2024-01-01T10:00',
-      end_date: '2024-12-31T18:00',
-      id_company: 1,
-      created_by: 1,
-      approver: [1]
-    };
-
-    setValue('NSAP', contract.NSAP);
-    setValue('DEN', contract.DEN);
-    setValue('project', contract.project);
-    setValue('API', contract.API);
-    // setValue('start_date', dayjs(contract.start_date).format('YYYY-MM-DDTHH:mm'));
-    // setValue('end_date', dayjs(contract.end_date).format('YYYY-MM-DDTHH:mm'));
-    setValue('id_company', contract.id_company);
-    setValue('created_by', contract.created_by);
-    setApprover(contract.approver);
-  }, [setValue]);
+  const { register, handleSubmit, formState: { errors }, control } = useForm();
+  const [selectedCompany, setSelectedCompany] = React.useState('');
+  const [selectedApprover, setSelectedApprover] = React.useState([]);
 
   const customSubmit = (data) => {
-    console.log({ ...data, approver });
-    alert(JSON.stringify({ ...data, approver }, null, 2));
+    onSubmit({ ...data, company_id: selectedCompany, approvers: selectedApprover });
   };
 
-  const handleChange = (event) => {
-    setApprover(event.target.value);
+  const handleCompanyChange = (event) => {
+    setSelectedCompany(event.target.value);
+  };
+
+  const handleApproverChange = (event) => {
+    setSelectedApprover(event.target.value);
   };
 
   return (
@@ -104,11 +52,13 @@ const ContractForm = ({ onSubmit }) => {
         error={!!errors.API}
         helperText={errors.API?.message}
       />
-      {/* <TextField
+      <TextField
         type="datetime-local"
         label="Start Date"
         {...register('start_date', { required: 'El campo no puede estar vacío' })}
-        InputLabelProps={{ shrink: true }}
+        InputLabelProps={{
+          shrink: true,
+        }}
         error={!!errors.start_date}
         helperText={errors.start_date?.message}
       />
@@ -116,49 +66,54 @@ const ContractForm = ({ onSubmit }) => {
         type="datetime-local"
         label="End Date"
         {...register('end_date', { required: 'El campo no puede estar vacío' })}
-        InputLabelProps={{ shrink: true }}
+        InputLabelProps={{
+          shrink: true,
+        }}
         error={!!errors.end_date}
         helperText={errors.end_date?.message}
-      /> */}
-      <FormControl md={{ m: 1, width: 300 }}>
+      />
+      <FormControl>
         <InputLabel id="company-label">Company</InputLabel>
         <Controller
-          name="id_company"
+          name="company"
           control={control}
-          rules={{ required: 'El campo no puede estar vacío' }}
+          defaultValue=""
           render={({ field }) => (
             <Select
               labelId="company-label"
-              id="id_company"
-              {...field}
+              id="company"
+              value={selectedCompany}
+              onChange={(e) => {
+                handleCompanyChange(e);
+                field.onChange(e.target.value);
+              }}
               input={<OutlinedInput label="Company" />}
             >
               {companies.map((company) => (
                 <MenuItem key={company.id} value={company.id}>
-                  {company.name}
+                  {company.name} ({company.rut_number}-{company.rut_verifier})
                 </MenuItem>
               ))}
             </Select>
           )}
         />
-        {errors.id_company && <p style={{ color: 'red' }}>{errors.id_company.message}</p>}
+        {errors.company && <p style={{ color: 'red' }}>{errors.company.message}</p>}
       </FormControl>
       <FormControl component="fieldset" sx={{ mb: 2 }}>
-        <InputLabel id="approver-label">Revisor otra área</InputLabel>
+        <InputLabel id="approver-label">Aprobador TCP</InputLabel>
         <Controller
           name="approver"
           control={control}
-        //   rules={{ required: isApproverRequired ? 'El campo no puede estar vacío' : false }}
           render={({ field }) => (
             <Select
               labelId="approver-label"
               id="approver"
-              value={approver}
+              value={selectedApprover}
               onChange={(e) => {
-                handleChange(e);
+                handleApproverChange(e);
                 field.onChange(e.target.value);
               }}
-              input={<OutlinedInput id="select-multiple-chip" label="Aprobador TCP" />}
+              input={<OutlinedInput label="Aprobador TCP" />}
               renderValue={(selected) => (
                 <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
                   {selected.map((value) => (
@@ -166,86 +121,19 @@ const ContractForm = ({ onSubmit }) => {
                   ))}
                 </Box>
               )}
-              MenuProps={MenuProps}
             >
               {users.map((user) => (
-                <MenuItem
-                  key={user.id}
-                  value={user.id}
-                  style={getStyles(user.name, approver.map(a => a.name), theme)}
-                >
-                  {user.name}
+                <MenuItem key={user.id} value={user.id}>
+                  {user.name} ({user.email})
                 </MenuItem>
               ))}
             </Select>
           )}
         />
         {errors.approver && <p style={{ color: 'red' }}>{errors.approver.message}</p>}
-        <FormControlLabel
-          control={
-            <Checkbox
-            //   checked={isApproverRequired}
-            //   onChange={(e) => setIsApproverRequired(e.target.checked)}
-              color="primary"
-            />
-          }
-          label="Obligatorio?"
-          sx={{ mt: 1 }}
-        />
       </FormControl>
-      <FormControl component="fieldset" sx={{ mb: 2 }}>
-        <InputLabel id="approver-label">Revisor CC</InputLabel>
-        <Controller
-          name="approver"
-          control={control}
-        //   rules={{ required: isApproverRequired ? 'El campo no puede estar vacío' : false }}
-          render={({ field }) => (
-            <Select
-              labelId="approver-label"
-              id="approver"
-              value={approver}
-              onChange={(e) => {
-                handleChange(e);
-                field.onChange(e.target.value);
-              }}
-              input={<OutlinedInput id="select-multiple-chip" label="Aprobador TCP" />}
-              renderValue={(selected) => (
-                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-                  {selected.map((value) => (
-                    <Chip key={value} label={users.find(user => user.id === value)?.name} />
-                  ))}
-                </Box>
-              )}
-              MenuProps={MenuProps}
-            >
-              {users.map((user) => (
-                <MenuItem
-                  key={user.id}
-                  value={user.id}
-                  style={getStyles(user.name, approver.map(a => a.name), theme)}
-                >
-                  {user.name}
-                </MenuItem>
-              ))}
-            </Select>
-          )}
-        />
-        {errors.approver && <p style={{ color: 'red' }}>{errors.approver.message}</p>}
-        <FormControlLabel
-          control={
-            <Checkbox
-            //   checked={isApproverRequired}
-            //   onChange={(e) => setIsApproverRequired(e.target.checked)}
-              color="primary"
-            />
-          }
-          label="Obligatorio?"
-          sx={{ mt: 1 }}
-        />
-      </FormControl>
-      
       <Button type="submit" variant="contained" color="primary">
-        {true ? 'Actualizar Contrato' : 'Crear Contrato'} {/* true para simular que ya hay un contrato */}
+        Crear Contrato
       </Button>
     </Box>
   );
--- src/pages/ContractFormPage.jsx
+++ src/pages/ContractFormPage.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ContractForm from '../Components/Contract/ContractForm';
-
+import axios from 'axios';
+import { BASE_URL } from '../helpers/config';
 
 const ContractFormPage = () => {
-  const handleCreateContract = () => {
-    // Lógica para crear un nuevo contrato
-    console.log('Crear contrato');
+  const [users, setUsers] = useState([]);
+  const [companies, setCompanies] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/getUsers`);
+        console.log('Usuarios obtenidos:', response.data); 
+        setUsers(response.data);
+      } catch (error) {
+        console.error('Error al obtener los usuarios', error);
+      }
+    };
+
+    const fetchCompanies = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/companies`);
+        console.log('Compañías obtenidas:', response.data); 
+        setCompanies(response.data);
+      } catch (error) {
+        console.error('Error al obtener las compañías', error);
+      }
+    };
+
+    fetchUsers();
+    fetchCompanies();
+  }, []);
+
+  const handleCreateContract = (data) => {
+    
+    console.log('Crear contrato', data);
+    
   };
 
   return (
     <div>
       <h2>Crear Contrato</h2>
-      <ContractForm onSubmit={handleCreateContract} /> {/* Renderiza el formulario */}
+      <ContractForm onSubmit={handleCreateContract} users={users} companies={companies} />
     </div>
   );
 };
