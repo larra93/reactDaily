f46d70da1097bd1c29a45a34e02fd0ac617785be
Felipe <felipelarra.araya@gmail.com>
2024-07-23 17:51:20 -0400

    Se agregan validación required dinamicas

--- src/Components/Containers/EECC/TableIngresarDaily.jsx
+++ src/Components/Containers/EECC/TableIngresarDaily.jsx
@@ -24,6 +24,7 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import axios from 'axios';
 import { BASE_URL } from '../../../helpers/config';
+import { toast } from 'react-toastify';
 
 const TableP = ({ fields, idSheet, idDaily }) => {
   const [validationErrors, setValidationErrors] = useState({});
@@ -41,7 +42,6 @@ const ListTypes = [
     if (!fields) return [];
 
     const rowMap = {};
-    console.log('aer', fields)
     fields.forEach(field => {
       field.values.forEach(value => {
         if (!rowMap[value.row]) {
@@ -57,7 +57,6 @@ const ListTypes = [
 
   const columns = useMemo(() => {
     const safeFields = fields || [];
-    console.log('save', safeFields)
     const safeValidationErrors = validationErrors || {};
     return safeFields.map((field) => {
 
@@ -97,69 +96,82 @@ const ListTypes = [
     isFetching: isFetchingUsers,
     isLoading: isLoadingUsers,
   } = useGetFields(idDaily, idSheet);
-  console.log('fetchedUsers:', fetchedUsers);
-  console.log('isFetchingUsers:', isFetchingUsers);
-  console.log('idSheet:', idSheet);
+
 
   const handleCreateField = async ({ values, table }) => {
-console.log('valll', values)
-console.log('requiredallsave', fetchedUsers.requiredAll)
-   console.log('fetchedUsers', fetchedUsers.rows)
-
+    
     const rowId = table.getState().creatingRowId;
     const maxRow = Math.max(...fetchedUsers.rows.map(row => row.id), 0);
     const newRowId = maxRow + 1;
-
-    const transformedValues = Object.keys(values).map((key) => {
-      const field = fields.find((f) => f.name === key);
-      console.log('field', field)
-      if (!field) {
+  
+    const transformedValues = fetchedUsers.requiredAll.map((field) => {
+      console.log('field111', field.name)
+      const value = values[field.name];
+      if (value === undefined) {
         return null;
       }
       return {
-        field_id: field ? field.id : null,
-        value: values[key],
-        daily_sheet_id: idSheet,
+        field_id: field.id,
+        value: value,
+        daily_sheet_id: field.daily_sheet_id,
         daily_id: idDaily,
         row: newRowId,
         required: field.required,
         name: field.name
       };
     }).filter(value => value !== null);
-      
-    const allRequiredFields = fields.flatMap(field => field).filter(field => field.required === 'Si');
-    console.log('allRequiredFields', allRequiredFields)
-    // const newValidationErrors = validateField(fetchedUsers.requiredAll);
+  
     const newValidationErrors = validateField(fetchedUsers.requiredAll, values);
+  
 
     if (Object.values(newValidationErrors).some((error) => error)) {
       setValidationErrors(newValidationErrors);
       return;
     }
-    console.log('transf', transformedValues)
+  
+    toast.success('Campos creados exitosamente');
     await createField(transformedValues);
     table.setCreatingRow(null);
   };
+  
 
   const handleSaveField = async ({ values, row, table }) => {
     const rowId = row.id;
-    const transformedValues = Object.keys(values).map((key) => {
-      console.log('valuesaaa', values)
-      const field = fields.find((f) => f.name === key);
-      const existingValue = field.values.find((v) => v.row === rowId);
+  
+    const transformedValues = fetchedUsers.requiredAll.map((field) => {
+      const value = values[field.name];
+  
+      if (value === undefined) {
+        return null;
+      }
+
+      const idValue = field.values.find(v => v.field_id === field.id && v.row === rowId);
+      
       return {
-        id: existingValue.id,
-        field_id: field ? field.id : null,
-        value: values[key],
-        daily_sheet_id: idSheet,
+        field_id: field.id,
+        value: value,
+        daily_sheet_id: field.daily_sheet_id,
         daily_id: idDaily,
-        row: rowId
+        row: rowId,
+        required: field.required,
+        name: field.name,
+        id: idValue.id
       };
-    });
+    }).filter(value => value !== null);
+  
+    
+    const newValidationErrors = validateCurrentSheetFields(idSheet,fetchedUsers.requiredAll, values);
+  
+    
+    if (Object.values(newValidationErrors).some((error) => error)) {
+      setValidationErrors(newValidationErrors);
+      return;
+    }
+  
     await updateField(transformedValues);
+    toast.success('Actualizado exitosamente');
     table.setEditingRow(null);
   };
-
   const openDeleteConfirmModal = (row) => {
     if (window.confirm('¿Estás seguro de que quieres eliminar este campo?')) {
       deleteField(row.original.id);
@@ -238,16 +250,16 @@ function useGetFields(idDaily, idSheet) {
 
       const response = await axios.get(`${BASE_URL}/Dailys/${idDaily}/dailyStructure`)
       var  fields = response.data.steps.find(step => step.idSheet === idSheet).fields;
-      console.log('fieldssssss222', fields)
       const steps = response.data.steps;
-      const requiredAll = steps.flatMap(step => step.fields);
-      console.log('requiredAll', requiredAll)
-      console.log('response.data.steps.', response.data.steps)
+      const allFields = steps.flatMap(step => step.fields);
+      
+
+      
       if (!fields) return [];
 
       const rowMap = {};
 
-      fields.forEach(field => {
+      allFields .forEach(field => {
         field.values.forEach(value => {
           if (!rowMap[value.row]) {
             rowMap[value.row] = { id: value.row };
@@ -255,25 +267,11 @@ function useGetFields(idDaily, idSheet) {
           rowMap[value.row][field.name] = value.value;
         });
       });
-      console.log('rowMap', rowMap)
+      
       return {
-        requiredAll,
+        requiredAll: allFields ,
         rows: Object.values(rowMap)
       };
-
-      // return Object.values(rowMap);
-
-
-
-
-
-      // const response = await axios.get(`${BASE_URL}/contracts/${idContract}/dailySheet`)
-      //obtengo los fields del dailysheet correspondiente y los ordeno segun su step
-      // const step = response.data.steps.find(step => step.idSheet === idSheet);
-      //  const fields = step.fields.sort((a, b) => a.step - b.step);
-
-      //return fields;
-
 
     },
     refetchOnWindowFocus: false,
@@ -301,7 +299,6 @@ function useUpdateField() {
 
   return useMutation({
     mutationFn: async (field) => {
-      console.log('updata', field);
       const response = await axios.put(`${BASE_URL}/updateValues`, field);
       return response.data;
     },
@@ -342,10 +339,8 @@ function useDeleteField() {
 const queryClient = new QueryClient();
 
 const Table = ({ data, idDaily }) => {
-  //  console.log('daily1', idDaily);
   if (!data) return null;
   const fields = data.fields.sort((a, b) => a.step - b.step);
-  console.log('daa', data)
   const idSheet = data.idSheet;
 
   return (
@@ -366,19 +361,8 @@ const validateEmail = (email) =>
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     );
 
-// function validateField(Field) {
-//   return {
-//     RUT: !validateRequired(Field.RUT)
-//       ? 'First Name is Required'
-//       : '',
-//    // lastName: !validateRequired(Field.lastName) ? 'Last Name is Required' : '',
-//     //state: !validateRequired(Field.state) ? 'Last Name is Required' : '',
-//   };
-// }
-
 function validateField(fields, values) {
   const validationErrors = {};
-
   fields.forEach(field => {
     if (field.required === 'Si' && !values[field.name]) {
       validationErrors[field.name] = `${field.name} es requerido`;
@@ -392,32 +376,25 @@ function validateField(fields, values) {
   return validationErrors;
 }
 
+function validateCurrentSheetFields(currentSheetId, allFields, values) {
+  const validationErrors = {};
 
+  // Filtrar los campos que pertenecen al idSheet actual
+  const currentSheetFields = allFields.filter(field => field.daily_sheet_id === currentSheetId);
 
-// function validateField(values) {
-//   const validationErrors = {};
-// console.log('valuessss', values)
-//   values.forEach(field => {
-//     console.log('values, field', field);
-//     if (field.required === 'Si') {
-//       validationErrors[field.name] = `${field.name} es requerido`;
-//     }
-//   });
-// console.log('validationErrors ', validationErrors )
-//   return validationErrors;
-// }
+  // Validar los campos filtrados
+  currentSheetFields.forEach(field => {
+    if (field.required === 'Si' && !values[field.name]) {
+      validationErrors[field.name] = `${field.name} es requerido`;
+    } else {
+      if (validationErrors[field.name]) {
+        delete validationErrors[field.name];
+      }
+    }
+  });
 
-// function validateField(values) {
-//   const validationErrors = {};
-
-//   Object.keys(values).forEach(key => {
-//     if (!values[key].trim()) {
-//       validationErrors[key] = `${key} es requerido`;
-//     }
-//   });
-
-//   return validationErrors;
-// }
+  return validationErrors;
+}
 
 
 
